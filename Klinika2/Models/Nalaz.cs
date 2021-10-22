db165a06ff69b9563b28d047946179da4f237b99
Sara Spahic <sara_spahic@yahoo.com>
2021-10-22 11:46:19 +0200

    Added Nalaz

---
+++ Klinika2/Models/Nalaz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Klinika2.Models
+{
+    public class Nalaz
+    {
+       // public int Id { get; set; }
+
+        [Key]
+        public int PrijemID { get; set; }
+
+        public string Opis { get; set; }
+
+        public DateTime DatumVrijeme { get; set; }
+    }
+}
